99f02a1195e3f1948bf432cb00ee160e3ad8db12
Lazariuc Maxim <lazariucmaxim@yandex.ru>
2024-07-04 12:30:57 +0300

    Task 2.1/work_with_database completed

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=30)
+    price = models.IntegerField()
+    image = models.URLField(max_length=200)
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.CharField(max_length=30)
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from phones.models import Phone
 
 
 def index(request):
@@ -7,11 +8,20 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    parameter = request.GET.get('sort')
+    phones = list(Phone.objects.all())
+    if parameter == 'name':
+        phones.sort(key=lambda x: x.name)
+    elif parameter == 'min_price':
+        phones.sort(key=lambda x: x.price)
+    elif parameter == 'max_price':
+        phones.sort(key=lambda x: x.price, reverse=True)
+    context = {'phones': phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.filter(slug=slug)[0]
+    context = {'phone': phone}
     return render(request, template, context)
