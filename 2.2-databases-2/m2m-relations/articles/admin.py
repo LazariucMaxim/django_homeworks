75cf2b09fe6e0be406d877612346f7df6bd1cb82
Lazariuc Maxim <lazariucmaxim@yandex.ru>
2024-07-11 21:09:42 +0300

    Task 2.2/m2m-relations completed

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Scope, Tag
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count_of_main_tags = sum(map(lambda form: form.cleaned_data.get('is_main', False), self.forms))
+        if count_of_main_tags == 0:
+            raise ValidationError('Укажите основной раздел')
+        elif count_of_main_tags > 1:
+            raise ValidationError('Основным может быть только один раздел')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
