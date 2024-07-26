7c253b1d593d3affc7cb9dce4d5e1b2f027d1416
Lazariuc Maxim <lazariucmaxim@yandex.ru>
2024-07-26 13:16:32 +0300

    Task 3.1/smart_home completed

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements')
+    temperature = models.DecimalField(max_digits=3, decimal_places=1)
+    created_at = models.TimeField(auto_now_add=True)
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,39 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from measurement.models import Sensor, Measurement
+from measurement.serializers import SensorBriefSerializer, SensorDetailSerializer
+
+
+class SensorView(APIView):
+    def get(self, request, pk=0):
+        if pk:
+            sensor = Sensor.objects.filter(pk=pk)[0]
+            ser = SensorDetailSerializer(sensor)
+        else:
+            sensors = Sensor.objects.all()
+            ser = SensorBriefSerializer(sensors, many=True)
+        return Response(ser.data)
+
+    def post(self, request):
+        data = request.data
+        Sensor(name=data.get('name'), description=data.get('description')).save()
+        return Response({'status': 'OK'})
+
+    def patch(self, request, pk):
+        sensor = Sensor.objects.filter(pk=pk)[0]
+        if 'name' in request.data:
+            sensor.name = request.data['name']
+        if 'description' in request.data:
+            sensor.description = request.data['description']
+        sensor.save()
+        return Response({'status': 'OK'})
+
+
+class MeasurementView(APIView):
+    def post(self, request):
+        data = request.data
+        Measurement(sensor_id=data.get('sensor'), temperature=data.get('temperature')).save()
+        return Response({'status': 'OK'})
