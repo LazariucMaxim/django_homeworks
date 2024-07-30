968a8be1edddfc5563ecba8e2487bf559e6e0003
Lazariuc Maxim <lazariucmaxim@yandex.ru>
2024-07-30 13:41:09 +0300

    Task 3.2/logistic completed

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -8,9 +8,12 @@ class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # при необходимости добавьте параметры фильтрации
+    filterset_fields = ['title']
+    search_fields = ['description']
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     # при необходимости добавьте параметры фильтрации
+    filterset_fields = ['products']
